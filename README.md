bf700501e39ed780e48bd1093ac3b30f0f401369
DouglasWu <dgrey1116@gmail.com>
2017-06-19 12:42:56 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-app-with-server
+
+Based on [this tutorial](https://www.fullstackreact.com/articles/using-create-react-app-with-a-server/)
---
+++ server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const app = express();
+
+const port = process.env.PORT || 3001;
+
+app.get('/api', (req, res) => {
+  res.json({
+    message: 'test'
+  });
+});
+
+// Express only serves static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+app.listen(port, () => {
+  console.log(`The server is running at http://localhost:${port}/`);
+});
